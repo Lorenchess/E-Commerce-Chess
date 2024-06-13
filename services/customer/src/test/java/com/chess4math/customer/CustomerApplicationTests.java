b34278208d5af3e6b124872d5338c5d83ab8618d
Ramon Lorente <ramon_lorente@yahoo.com>
2024-06-13 05:23:27 -0400

    initial configuration of docker, eureka server and config server

---
+++ services/customer/src/test/java/com/chess4math/customer/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chess4math.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ services/discovery/src/test/java/com/chess4math/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chess4math.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
